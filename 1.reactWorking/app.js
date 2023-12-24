695ba3cc91c775bb8b45149a8a5aad5d628487b1
Vishal Dogra <dogravishal1598@gmail.com>
2023-12-24 09:59:17 +0530

    added JSX into code

--- 1.reactWorking/app.js
+++ 1.reactWorking/app.js
@@ -27,24 +27,22 @@ import ReactDOM from "react-dom/client";
  *
  *
  */
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "h1 tag"),
-        React.createElement("h2", {}, "h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "h1 tag"),
-        React.createElement("h2", {}, "h2 tag"),
-    ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1" }, [
+//         React.createElement("h1", {}, "h1 tag"),
+//         React.createElement("h2", {}, "h2 tag"),
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//         React.createElement("h1", {}, "h1 tag"),
+//         React.createElement("h2", {}, "h2 tag"),
+//     ]),
+// ]);
 
-console.log(parent);
+// console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
-//
+// root.render(parent);
 
 // const heading = React.createElement(
 //     "h1",
@@ -57,3 +55,62 @@ root.render(parent);
 // const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 
 // root2.render(heading);
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// React.createElement    ==>    ReactElement (JS Object)    ==>    HTMLElement (render)
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello react!");
+// console.log(heading);
+
+// JSX    ==>    Babel transpiles to React.createElement    ==>    ReactElement (JS Object)    ==>    HTMLElement (render)
+
+// JSX = HTML-like syntax
+// this is not valid pure js, js engine and browsers ony understands ecmascript
+// this code will give syntax error in pure javascript
+// this code is transpiled by parcel(using babel) before it is handed over to the js engine
+// if we are writing JSX in multiple lines then "()" are required, for one line of code "()" are optional
+// const jsxHeading = (
+//     <h1 id="heading" className="head">
+//         Hello React using JSX!
+//     </h1>
+// );
+// console.log(jsxHeading);
+
+// React Functional Component
+// it is a JS function whcih returns some piece of JSX code
+function Title() {
+    return (
+        <h1 id="heading" className="head">
+            Title Component!
+        </h1>
+    );
+}
+
+const headingElement = (
+    <h1 id="heading" className="head">
+        Heading Element
+    </h1>
+);
+
+const HeadingComponent = () => {
+    return (
+        <div>
+            {/* this is componenet composition - using a component inside another component */}
+            {/* below 3 statements are same */}
+            <Title />
+            <Title></Title>
+            {Title()}
+            {headingElement}
+            <h1>Hello React Functional Component!</h1>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// render a react element
+// root.render(jsxHeading);
+
+// render a functional component
+root.render(<HeadingComponent />);
