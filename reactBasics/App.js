92f1a64b095e451f7fd6eb410a788efdc93c8f67
Vishal Dogra <dogravishal1598@gmail.com>
2024-03-04 19:17:38 +0530

    JSX and react functional components

--- reactBasics/App.js
+++ reactBasics/App.js
@@ -11,10 +11,7 @@
 
 // --------------------------------------------------------------------------------------------------------------------------------------
 
-// Using React
-import React from "react";
-import ReactDOM from "react-dom/client";
-
+// Using react with cdn
 /*
 	element creation is handled by react
 	rendering of elements created by react is handled by reactDOM
@@ -33,6 +30,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading); // this render method is responsible for converting the react element into HTML element and render it on the screen
 */
 
+/*
 const parent = React.createElement(
 	"div",
 	{ id: "parent" },
@@ -44,5 +42,45 @@ const parent = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+*/
 
 // --------------------------------------------------------------------------------------------------------------------------------------
+
+// Using react as package module
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// creating react element using JSX
+// JSX is XML-like sytax
+// JSX code is transpiled before it reaches JS engine
+// Babel(Js transpiler) is doing the transpiling job, and Babel is used by parcel
+// JSX	=>	transpiled to React.creatElement
+// to write JSX in multiple lines we need to wrap it with ()
+const heading = <h1 className="heading">Hello World, using JSX</h1>;
+
+/*
+	React Functional Components
+	===========================
+	Just a normal Js function which return some JSX.
+
+	**Note: React functional components name shoulf start with capital(it is a way for react to understand component)
+
+	Component composition: Combining components together to create larger components.
+*/
+// It is not compulsory to use arrow functions to create react functional components(but it is recommended)
+const Title = () => <h1>Title Component</h1>;
+
+const HeadingComponent = () => {
+	return (
+		<div className="container">
+			<h1 className="heading">Heading Component</h1>
+			{/* below 3 statements are same */}
+			<Title />
+			<Title></Title>
+			{Title()}
+		</div>
+	);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
